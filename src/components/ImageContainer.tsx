5abe7e4b1eec221f865ad3480855eb6f2f9ecb63
Daris02 <hei.raymond.2@gmail.com>
2024-09-23 10:15:39 +0300

    refactor: change default image directory

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -2,7 +2,7 @@
 function ImageContainer () {
   return (
     <div className="flex items-center">
-      <img src="/src/assets/img-1.png" alt="Placeholder" className="w-full h-full mr-2" />
+      <img src="/assets/img-1.png" alt="Placeholder" className="w-full h-full mr-2" />
     </div>
   );
 };
